5d7cac4ee26d6fbac786d102e54d93db89ec38c9
aleks20905 <aleks_20905@mail.bg>
2023-12-03 23:18:52 +0200

    some zad

---
+++ project2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main (){    
+   for (int i = 1; i <= 12; i++)
+   {
+    for (int a = 1; a <= 12; a++)
+    {
+        cout<<i*a<<"\t";
+    }
+    cout<<endl;
+   }
+   
+   
+    
+    
+
+    return 0;
+}
