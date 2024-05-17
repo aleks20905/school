994d4998e93ae701b5c0cc8dc194cc5a00ef54cc
aleks <aleks_20905@mail.bg>
2024-05-17 19:37:03 +0300

    added benches

---
+++ benches/linkList/link.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using ms = chrono::milliseconds;
+
+struct Node {
+    Node *next;
+    float val;
+};
+
+Node *nodes = nullptr;
+
+void iterateAndPlace(double val) {
+    Node *node = nodes;
+    Node *prev = nullptr;
+
+    while (node != nullptr) {
+        if (node->val > 99999) {
+            break;
+        }
+
+        prev = node;
+        node = node->next;
+    }
+
+    prev->val = val;
+}
+
+int main() {
+    Node x = {};
+    cout << "Size of struct: " << sizeof(x) << "\n"; // 16 bytes
+
+    // Fill in global linked list with 3000 dummy nodes
+    for (int i=0; i<3000; i++) {
+        Node* newNode = new Node;
+        newNode->val = 0.0;
+        newNode->next = nodes;
+        nodes = newNode;
+    }
+
+    for (int retrys = 50; retrys < 1000000; retrys+=retrys*0.15) {
+    auto start = chrono::steady_clock::now();
+    for (int i=0; i<retrys; i++) {
+        iterateAndPlace(100.1);
+    }
+
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    // cout <<"size : "<<retrys<<" \t time :  "<< chrono::duration_cast<ms>(diff).count()<<" ms "<<endl;
+    cout << chrono::duration_cast<ms>(diff).count()<<endl;
+
+    }
+       
+    
+}       
---
+++ benches/linkList/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"unsafe"
+)
+
+type Node struct {
+	next *Node
+	val  float64
+}
+
+var nodes *Node = nil
+
+func iterateAndPlace(val float64) {
+	node := nodes
+	var prev *Node = nil
+
+	for node != nil {
+		if node.val > 99999 {
+			break
+		}
+		prev = node
+		node = node.next
+	}
+
+	prev.val = val
+}
+
+func main() {
+	x := Node{}
+	fmt.Printf("Size of struct: %d\n", unsafe.Sizeof(x)) // 16 bytes
+
+	for i := 0; i < 3000; i++ {
+		newNode := new(Node)
+		newNode.next = nodes
+		nodes = newNode
+	}
+
+	for retrys := 50; retrys < 1000000; retrys += int(float32(retrys) * 0.15) {
+		start := time.Now()
+		for i := 0; i < retrys; i++ {
+			iterateAndPlace(100.1)
+		}
+		//fmt.Printf(" %v Time elapsed: %v\n", retrys, time.Since(start).Milliseconds())
+		fmt.Printf("%v\n", time.Since(start).Milliseconds())
+
+	}
+
+}
