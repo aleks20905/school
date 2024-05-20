c0618fd69c905658aa02ed1d8e02f7c29027e844
Bata Ti <aleks_20905@mail.bg>
2024-05-20 16:53:50 +0300

    update cPP

--- benches/hashmap/game.cpp
+++ benches/hashmap/game.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 
 int main() {
-    auto start = chrono::steady_clock::now();
-
+    
     unordered_map<double, double> hm;
-    double c = 0.0;
+    
+    auto start = chrono::steady_clock::now();
+    
     for (int i = 0; i < 10000000; i++) {
-        hm[c] = c + 10.0;
-        c += 1.0;
+        hm[i] = i + 10.0;
     }
 
     auto end = chrono::steady_clock::now();
@@ -21,7 +21,7 @@ int main() {
 
 
 
-// Time : 11729 ms
+// Time : 6912 ms
 
 
 }
