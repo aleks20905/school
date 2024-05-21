40279f80686ddb99dc8c8fa1371e4aad0ca2319d
aleks <aleks_20905@mail.bg>
2024-05-21 22:15:18 +0300

    hashmap update

--- benches/hashmap/hashmap.cpp
+++ benches/hashmap/hashmap.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <unordered_map>
 #include <chrono>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
 
 using namespace std;
 
 
+void printMemoryUsage() { //са мо за Linix 
+    ifstream file("/proc/self/statm");
+    if (!file.is_open()) {
+        cerr << "Error: Could not open /proc/self/statm" << endl;
+        return;
+    }
+    long size, resident, share, text, lib, data, dt;
+    file >> size >> resident >> share >> text >> lib >> data >> dt;
+    file.close();
+
+    long pageSize = sysconf(_SC_PAGESIZE); // In bytes 
+    long residentSize = resident * pageSize;
+
+    cout << "Memory Usage: " << residentSize / 1024 << " KB" << endl; // Convert to KB
+}
+
 int main() {
     
     unordered_map<double, double> hm;
+    printMemoryUsage();
+    // Memory Usage: 3328 KB
     
     auto start = chrono::steady_clock::now();
     
@@ -18,8 +39,10 @@ int main() {
     auto end = chrono::steady_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
     cout << "create Time : " << duration << " ms" << endl;
+    printMemoryUsage();
 
 // Time : 7697 ms
+// Memory Usage: 410560 KB
 
     start = chrono::steady_clock::now();
     
@@ -30,7 +53,9 @@ int main() {
     end = chrono::steady_clock::now();
     duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
     cout << "modify Time : " << duration << " ms" << endl;
+    printMemoryUsage();
 
 // Time : 4697 ms
+// Memory Usage: 410560 KB
 
 }
--- benches/hashmap/main.go
+++ benches/hashmap/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"syscall"
 	"time"
 )
+
+func getMemoryUsage() uint64 {
+	data, err := ioutil.ReadFile("/proc/self/statm")
+	if err != nil {
+		fmt.Println("Error: Could not open /proc/self/statm")
+		return 0
+	}
+
+	fields := string(data)
+	var size, resident, share, text, lib, dataValue, dt uint64
+	fmt.Sscanf(fields, "%d %d %d %d %d %d %d", &size, &resident, &share, &text, &lib, &dataValue, &dt)
+
+	pageSize := uint64(syscall.Getpagesize()) // In bytes
+	residentSize := resident * pageSize
+
+	return residentSize
+}
 
 func main() {
 
 	o := make(map[float64]float64)
+	fmt.Println("mem: ", getMemoryUsage())
+	// mem:  1835008
 
 	start := time.Now()
 	for i := 0.0; i <= 10000000; i++ {
@@ -15,8 +36,10 @@ func main() {
 	}
 
 	fmt.Println("create time: ", time.Since(start))
+	fmt.Println("mem: ", getMemoryUsage())
 
 	// time:  2.711069621s
+	// mem:  644612096
 
 	start = time.Now()
 	for i := 0.0; i <= 10000000; i++ {
@@ -24,6 +47,9 @@ func main() {
 	}
 
 	fmt.Println("modify time: ", time.Since(start))
+	fmt.Println("mem: ", getMemoryUsage())
+
 	// time:  1.638830667s
+	// mem:  644612096
 
 }
